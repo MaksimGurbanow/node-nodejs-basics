712c56e6632d0c6001cf616dc39acdae27936b05
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-30 00:58:23 +0300

    add: implement solution task for zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
-const compress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream, unlink } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
+const compress = async (src, dest) => {
+  const readableStream = createReadStream(src);
+  const gZip = createGzip();
+  const writableStream = createWriteStream(dest);
+
+  readableStream.pipe(gZip).pipe(writableStream);
+
+  writableStream.on("finish", () => {
+    unlink(src, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
 };
 
-await compress();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const src = path.join(__dirname, "files", "fileToCompress.txt");
+const dest = path.join(__dirname, "files", "archive.gz");
+await compress(src, dest);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream, unlink } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createUnzip } from "zlib";
+
+const decompress = async (src, dest) => {
+  const readableStream = createReadStream(src);
+  const unZip = createUnzip();
+  const writableStream = createWriteStream(dest);
+
+  readableStream.pipe(unZip).pipe(writableStream);
+
+  writableStream.on("finish", () => {
+    unlink(src, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
 };
 
-await decompress();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const src = path.join(__dirname, "files", "archive.gz");
+const dest = path.join(__dirname, "files", "fileToCompress.txt");
+await decompress(src, dest);
