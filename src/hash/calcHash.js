d4b00f9925bb9c8d56e93b777cc9f3ddaa5cf431
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-30 00:28:32 +0300

    add: implement solution task for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
-const calculateHash = async () => {
-    // Write your code here 
+import path from 'path';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import crypto from 'crypto';
+
+const calculateHash = async (src) => {
+	const readableStream = fs.createReadStream(src);
+	const hash = crypto.createHash('sha256');
+
+	readableStream.on('data', (data) => {
+		hash.update(data)
+	})
+
+	readableStream.on('end', () => {
+		stdout.write('Hash => ' + hash.digest('hex'));
+	})
+
+	readableStream.on('error', (err) => console.error(err));
 };
 
-await calculateHash();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const src = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+await calculateHash(src);
