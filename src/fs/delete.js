830d0da1a63ad918101693733d717ed04602e0cc
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-29 20:25:27 +0300

    add: implement solution task for fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
-const remove = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+
+const remove = async (filePath) => {
+	try {
+		await fs.access(filePath);
+
+		await fs.rm(filePath);
+} catch (error) {
+		if (error.code === 'ENOENT') {
+			console.error('FS operation failed');
+		}
+}
 };
 
-await remove();
+const filePath = path.join('src', 'fs', 'files', 'fileToRemove.txt');
+await remove(filePath);
