790898fe88cb13ac8c6122e823bafe24b83e25cd
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-29 22:54:51 +0300

    add: implement solution task for streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const read = async (readFrom) => {
+    try {
+        await fs.promises.access(readFrom);
+        const readableStream = fs.createReadStream(readFrom, { encoding: 'utf-8' });
+
+        readableStream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-await read();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const readFrom = path.join(__dirname, 'files', 'fileToRead.txt');
+await read(readFrom);
