22a0e6c1ebec917ba77963bedd19e1fac7bdc2b1
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-29 23:20:42 +0300

    add: implement solution task for streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,38 @@
-const write = async () => {
-    // Write your code here 
+import fs from "fs";
+import { stdin, stdout, exit } from 'process';
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const write = async (dest) => {
+  try {
+    await fs.promises.access(dest);
+		const input = fs.createWriteStream(dest);
+    stdout.write("Please enter the text: ");
+    stdin.on("data", (data) => {
+      if (data.toString().trim() === "exit") {
+        exit();
+      } else {
+        input.write(data, (err) => {
+          try {
+            stdout.write("The text was added: " + data);
+          } catch {
+            throw err;
+          }
+        });
+      }
+    });
+    process.on("exit", () => {
+      stdout.write("The program finished! Bye!");
+    });
+
+    process.on("SIGINT", () => {
+      exit();
+    });
+  } catch (error) {
+		console.error(error)
+	}
 };
 
-await write();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const writeTo = path.join(__dirname, "files", "fileToWrite.txt");
+await write(writeTo);
