190b4288602bac9a88e8ba574d5e4712354693b2
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-30 00:07:28 +0300

    add: implement solution task for streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { stdout, stdin } from "process";
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(chunk, __, callback) {
+      this.push(
+        `${chunk.toString().trim().split("").reverse().join("")}\n`
+      );
+      callback();
+    },
+  });
+
+	stdout.write("Please enter the text: ");
+	stdin.pipe(reverseTransform).pipe(stdout);
+
+	process.on('SIGINT', () => {
+    stdout.write(`\nThe program finished! Bye!`);
+    stdin.end();
+    process.exit(0);
+  });
 };
 
 await transform();
